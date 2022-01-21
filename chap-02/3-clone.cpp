8d45965c950710dc1d965e6c2ab8159a6e36b72c
Cyril VAN <cvan1@etud.u-pem.fr>
2022-01-21 10:34:56 +0100

    Chap 2

--- chap-02/3-clone.cpp
+++ chap-02/3-clone.cpp
@@ -4,10 +4,26 @@
 class Person
 {
 public:
+    Person(const Person& other)
+        : _name { other._name }
+        , _surname { other._surname }
+    {}
+
     Person(const std::string& name, const std::string& surname)
         : _name { name }
         , _surname { surname }
     {}
+
+    Person& operator=(const Person& other)
+    {
+        if (this != &other)
+        {
+            _name    = other._name;
+            _surname = other._surname;
+        }
+
+        return *this;
+    }
 
     std::string  get_full_name() const { return _name + " " + _surname; }
     unsigned int get_age() const { return _age; }
@@ -20,13 +36,27 @@ private:
     unsigned int _age = 0u;
 };
 
+std::ostream& operator<<(std::ostream& out, const Person& other)
+{
+    return out << "Person named '" << other.get_full_name() << "' is " << other.get_age() << " years old.";
+}
+
 int main()
 {
     Person batman { "Bruce", "Wayne" };
     batman.wait(23);
 
-    std::cout << "Person named '" << batman.get_full_name() << "' is " << batman.get_age() << " years old."
-              << std::endl;
+    std::cout << batman << std::endl;
+
+    Person copy = batman;
+
+    std::cout << copy << std::endl;
+
+    Person assigned_clone { "Batman", "2" };
+    std::cout << assigned_clone << std::endl;
+
+    assigned_clone = batman;
+    std::cout << assigned_clone << std::endl;
 
     return 0;
 }
--- chap-02/4-rectangle/Rectangle.h
+++ chap-02/4-rectangle/Rectangle.h
@@ -1,23 +1,26 @@
 #pragma once
+#include <iostream>
 
 class Rectangle
 {
 public:
-    Rectangle(float length, float width)
-        : _length { length }
-        , _width { width }
-    {}
+    Rectangle(float length, float width);
+
+    Rectangle(float side);
+
+    Rectangle();
 
     float get_length() const { return _length; }
     float get_width() const { return _width; }
 
-    void scale(float ratio)
-    {
-        _length *= ratio;
-        _width *= ratio;
-    }
+    void scale(float ratio);
+
+    static void set_default_size(float size);
 
 private:
-    float _length;
-    float _width;
+    static float _default_size;
+    float        _length;
+    float        _width;
 };
+
+std::ostream& operator<<(std::ostream& stream, const Rectangle& rectangle);
